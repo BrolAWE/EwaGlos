827262b189a10c7e83fa06459cf2e114d11cf16d
aleks <delov.alesha@yandex.ru>
2019-11-14 01:06:42 +0300

    Add View

--- EwaGlos/urls.py
+++ EwaGlos/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 
@@ -20,5 +21,5 @@ from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    url(r'^word/(?P<pk>\d+)/(?P<lan>\w+)/$', views.word, name="word"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,10 +1,21 @@
+from django.http import Http404
 from django.shortcuts import render
 
-
 # Create your views here.
+from core.models import *
 
-def index(request):
-    return render(
-        request,
-        'index.html'
-    )
+
+def word(request, pk, lan):
+    try:
+        word = Word.objects.get(pk=pk)
+        subsection = Subsection.objects.get(pk=word.subsection.pk)
+        section = Section.objects.get(pk=subsection.section.pk)
+        word_translation = WordTranslation.objects.get(word=pk, language=lan)
+    except Word.DoesNotExist:
+        raise Http404
+    return render(request, lan+'.html', context={
+        'word': word,
+        'subsection': subsection,
+        'section': section,
+        'word_translation': word_translation
+    })

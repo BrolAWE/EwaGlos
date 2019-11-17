0ae17f08816fca01d4968377e3847fc66fe50174
aleks <delov.alesha@yandex.ru>
2019-11-17 22:43:24 +0300

    fix models and admin

--- core/admin.py
+++ core/admin.py
@@ -3,9 +3,37 @@ from django.contrib import admin
 # Register your models here.
 from core.models import *
 
-admin.site.register(Section)
-admin.site.register(Subsection)
-admin.site.register(Word)
-admin.site.register(WordTranslation)
-admin.site.register(SectionTranslation)
-admin.site.register(SubsectionTranslation)
+
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    list_display = ("id", "name",)
+
+
+@admin.register(Subsection)
+class SubsectionAdmin(admin.ModelAdmin):
+    list_display = ("id", "name",)
+
+
+@admin.register(Word)
+class WordAdmin(admin.ModelAdmin):
+    list_display = ("id", "name",)
+
+
+@admin.register(WordTranslation)
+class WordTranslationAdmin(admin.ModelAdmin):
+    list_display = ("id", "name",)
+
+
+@admin.register(SectionTranslation)
+class SectionTranslationAdmin(admin.ModelAdmin):
+    list_display = ("id", "name",)
+
+
+@admin.register(SubsectionTranslation)
+class SubsectionTranslationAdmin(admin.ModelAdmin):
+    list_display = ("id", "name",)
+
+
+@admin.register(CloseSenseWord)
+class CloseSenseWordAdmin(admin.ModelAdmin):
+    list_display = ("word", "close_sense")
--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
 LANG = (
     ('BG', 'Bulgarian'),
     ('DE', 'German'),
@@ -17,38 +18,44 @@ LANG = (
 
 
 class Section(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    colour = models.CharField(max_length=10, default=None, help_text="Please use the following format: <em>#00FF00</em>.")
+    name = models.CharField(max_length=100, help_text="Use the following format: <em>Name of the section</em>.")
+    color = models.CharField(max_length=7, help_text="Use the following format: <em>#00FF00</em>.")
 
 
 class Subsection(models.Model):
-    section = models.ForeignKey(Section, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100, unique=True)
-    colour = models.CharField(max_length=10, help_text="Please use the following format: <em>#00FF00</em>.")
+    section = models.ForeignKey(Section, on_delete=models.CASCADE, db_column="section")
+    name = models.CharField(max_length=100, help_text="Use the following format: <em>Name of the subsection</em>.")
+    color = models.CharField(max_length=7, help_text="Use the following format: <em>#00FF00</em>.")
 
 
 class Word(models.Model):
     subsection = models.ForeignKey(Subsection, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100, unique=True)
-    definition = models.TextField()
-    comment = models.TextField()
+    name = models.CharField(max_length=100, help_text="Use the following format: <em>Word</em>.")
+    definition = models.TextField(help_text="Write down the whole definition.")
+    comment = models.TextField(help_text="Write some notes about the exhibit in the picture")
 
 
 class WordTranslation(models.Model):
     word = models.ForeignKey(Word, on_delete=models.CASCADE)
-    language = models.CharField(max_length=10, unique=True, choices=LANG)
-    name = models.CharField(max_length=300, primary_key=True)
-    definition = models.TextField()
-    comment = models.TextField()
+    language = models.CharField(max_length=2, choices=LANG, help_text="Choose the language from the list")
+    name = models.CharField(max_length=100, help_text="Write down the translation.")
+    definition = models.TextField(help_text="Write down the whole definition.")
+    comment = models.TextField(help_text="Write some notes about the exhibit in the picture")
 
 
 class SectionTranslation(models.Model):
     section = models.ForeignKey(Section, on_delete=models.CASCADE)
-    language = models.CharField(max_length=10, unique=True, choices=LANG)
-    name = models.CharField(max_length=100, primary_key=True)
+    language = models.CharField(max_length=2, choices=LANG)
+    name = models.CharField(max_length=100)
 
 
 class SubsectionTranslation(models.Model):
     subsection = models.ForeignKey(Subsection, on_delete=models.CASCADE)
-    language = models.CharField(max_length=10, unique=True, choices=LANG)
-    name = models.CharField(max_length=100, primary_key=True)
+    language = models.CharField(max_length=2, choices=LANG)
+    name = models.CharField(max_length=100)
+
+
+class CloseSenseWord(models.Model):
+    word = models.ForeignKey(Word, on_delete=models.CASCADE, help_text="Choose from the list the word")
+    close_sense = models.ForeignKey(WordTranslation, on_delete=models.CASCADE,
+                                    help_text="Choose from the list close sense word")
